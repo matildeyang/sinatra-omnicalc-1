d795380c27eb2aa998b2ce148b23698d8cf96bae
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-23 18:13:23 +0000

    square root

--- app.rb
+++ app.rb
@@ -10,10 +10,19 @@ get("/square/new") do
 end 
 
 get("/square/results") do 
+  @the_num = params.fetch("num")
+  @the_result = @the_num.to_f**2
   erb(:square_results)
 end
 
 get("/square_root/new") do
+  erb(:new_square_root)
+end 
+
+get("/square_root/results") do 
+  @the_num = params.fetch("num")
+  @the_result = @the_num.to_f ** 0.5
+  erb(:square_root_results)
 end 
 
 get("/payment/new") do 
